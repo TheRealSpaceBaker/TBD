adb589874b8e579e447942d2cf7e14c255beeb92
KingCaellum <2104153fijneman@zuyd.nl>
2024-06-19 22:39:20 +0200

    Klassen aangemaakt. In de klasse Doctor de mathode CancelAppointment toegevoegd, in Appointment description en patientid toegevoegd en bij CMAS de methode AddScore toegevoegd.

---
+++ TBD-Console/Appointment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBD_Console
+{
+    public class Appointment : CMAS
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Patient PatientId { get; set; }
+        public Doctor DoctorId { get; set; }
+
+        public Appointment(int id, DateTime date, string description, Patient patientId, Doctor doctorId)
+            : base(id, patientId)
+        {
+            Id = id;
+            Date = date;
+            Description = description;
+            PatientId = patientId;
+            DoctorId = doctorId;
+
+        }
+
+        public void FillInData()
+        {
+
+        }
+
+    }
+}
---
+++ TBD-Console/CMASExercise.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBD_Console
+{
+    public class CMASExercise
+    {
+        public int Id { get; set; }
+        public Exercise Exercise { get; set; }
+        public int Score { get; set; }
+
+        public CMASExercise(int id, Exercise exercise, int score)
+        {
+            Id = id;
+            Exercise = exercise;
+            Score = score;
+        }
+
+        public static List<CMASExercise> ShowCMASExercise()
+        {
+            return null; //Dal methode aanroepen
+        }
+
+        public void DoCMASExercise()
+        {
+
+        }
+    }
+}
