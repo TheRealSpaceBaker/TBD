68940502bf9edb1d4e35215a311a99f23ba28329
dnsvh <2205686herten@zuyd.nl>
2024-06-21 10:23:26 +0200

    Added all windows and pages

---
+++ TBD-Innovatie/Pages/P_Level.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TBD_Innovatie.Pages
+{
+    /// <summary>
+    /// Interaction logic for P_Level.xaml
+    /// </summary>
+    public partial class P_Level : Page
+    {
+        public P_Level()
+        {
+            InitializeComponent();
+        }
+    }
+}
